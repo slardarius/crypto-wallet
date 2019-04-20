9b65a49f3997ae7053643ddc4566f6c8a15a3eb7
Artem <artem.zapaschikov@agilie.com>
2019-04-20 14:42:32 +0300

    add new page -> welcome.

---
+++ app/components/Navigate.vue
@@ -0,0 +1,18 @@
+<template>
+    <Page class="test">
+        <StackLayout>
+            <Label class="message" text="Tab 3 Contentasdfasdfasdfasdfsadfsadf" col="0" row="0"/>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+    name: 'app-navigate-component',
+    deta() {
+        return {
+
+        }
+    }
+}
+</script>
